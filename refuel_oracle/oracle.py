81a3091e26c6e85d8c91f692302c68580d6a5932
Patrick Cramer <pcramer247@gmail.com>
2023-03-28 13:04:42 -0500

    finish annotate function, and add example dataset to test on in examples/ directory

--- refuel_oracle/oracle.py
+++ refuel_oracle/oracle.py
@@ -3,6 +3,10 @@ from refuel_oracle.llm import LLM, LLMProvider, LLMResults, OpenAI
 import json
 import pprint
 import pandas as pd
+import numpy as np
+import tiktoken
+
+CHUNK_SIZE = 5
 
 
 def get_num_tokens_from_string(string: str, text_model: str) -> int:
@@ -47,39 +51,61 @@ class Oracle:
         ground_truth_column=None,
         verbose: bool = False,
         n_trials: int = 1,
+        max_items: int = 100,
     ):
         dat = pd.read_csv(dataset)
 
         input = dat[input_column].tolist()
         truth = None if not ground_truth_column else dat[ground_truth_column].tolist()
 
-        annotation_instruction = self.instruction
-        examples = self.seed_examples
-        prompt = self.prompt
-        final_prompt = f"""{annotation_instruction}\n{prompt}\n{examples}"""
-
-        num_tokens = get_num_tokens_from_string(final_prompt, self.model_name)
-        total_tokens, token_counter = 0, 0
-        total_tokens += num_tokens
-        token_counter += num_tokens
-
         yes_or_no = []
         llm_labels = []
-        if token_counter > MAX_TOKENS_PER_REQUEST:
-            # calling OpenAI and parsing the response
+        prompt_list = []
+        total_tokens = 0
+
+        max_items = min(max_items, len(input))
+        num_sections = max(max_items / CHUNK_SIZE, 1)
+        for chunk_id, chunk in enumerate(
+            np.array_split(input[:max_items], num_sections)
+        ):
+            for i, input_i in enumerate(chunk):
+                if (i + 1) % 10 == 0:
+                    print(f"{i+1}/{len(input)}...")
+
+                # Construct Prompt to pass to LLM
+                annotation_instruction = self.instruction
+                annotation_instruction += (
+                    f"\n\nDo you know the answer(YES/NO):\nCategories:\n"
+                )
+                for label_category in self.labels:
+                    annotation_instruction += f"{label_category}\n"
+                examples = self.seed_examples
+                examples_string = (
+                    "Some examples with their categories are provided below:\n\n"
+                )
+                for example in examples:
+                    examples_string += f"Example:\n{example['example']}\n\nDo you know the answer(YES/NO):\n"
+                    examples_string += (
+                        f"{example['yes_or_no']}\nCATEGORY:\n{example['label']}\n\n"
+                    )
+                prompt = self.prompt
+                instruction = prompt.replace("{example}", input_i)
+                final_prompt = (
+                    f"""{annotation_instruction}\n{examples_string}\n{instruction}"""
+                )
+                num_tokens = get_num_tokens_from_string(final_prompt, self.model_name)
+                total_tokens += num_tokens
+                prompt_list.append(final_prompt)
+            # Get response from LLM
             response = self.llm.generate(prompt_list)
-            for response_item in response.choices:
-                parts = response_item.text.split("\n")
+            for response_item in response.completions:
+                parts = response_item["text"].split("\n")
                 yes_or_no.append(parts[0])
                 llm_labels.append(parts[-1])
 
-            # refreshing list
-            token_counter = num_tokens
-            prompt_list = [final_prompt]
-        else:
-            prompt_list.append(final_prompt)
-
         # Write output to CSV
+        if len(llm_labels) < len(dat):
+            llm_labels = llm_labels + [None for i in range(len(dat) - len(llm_labels))]
         dat[output_column] = llm_labels
         dat.to_csv(output_dataset)
         return
--- test.py
+++ test.py
@@ -5,7 +5,8 @@ config_path = "examples/config.json"
 annotator = Oracle(config_path)
 
 annotator.annotate(
-    dataset="/Users/pc/refuel/repos/refuel-oracle/refuel_oracle/ag_news_filtered_labels_sampled.csv",
+    dataset="/Users/pc/refuel/repos/refuel-oracle/examples/ag_news_filtered_labels_sampled.csv",
     input_column="description",
     output_column="llm_labels",
+    max_items=10,
 )
