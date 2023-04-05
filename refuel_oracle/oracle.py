cc75671f25ee8feff2b3572c259c7109cbbe5d60
Nihit <nihit@cs.stanford.edu>
2023-04-05 14:44:51 -0700

    Move evaluation to task class (#13)
    * Move evaluation to task class
    
    * more changes

--- refuel_oracle/oracle.py
+++ refuel_oracle/oracle.py
@@ -1,45 +1,17 @@
 import pandas as pd
 import numpy as np
-from typing import List
-from collections import Counter
 
 from refuel_oracle.llm import LLMFactory
 from refuel_oracle.config import Config
 from refuel_oracle.tasks import TaskFactory
-from refuel_oracle.schema import LLMAnnotation
 from refuel_oracle.utils import (
     calculate_num_tokens,
     calculate_cost
 )
 
-CHUNK_SIZE = 5
-
-def evaluate(true_labels: List, pred_labels: List, verbose: bool = False):
-    # Calculate accuracy of predictions vs provided true labels
-    lower_case_true_labels = [s.lower() for s in true_labels]
-    true_distribution = Counter(lower_case_true_labels)
-    if verbose:
-        print("Ground truth label distribution:")
-        print(true_distribution)
-        print("#" * 80)
-
-    lower_case_preds = [s.lower() for s in pred_labels]
-    preds_distribution = Counter(lower_case_preds)
-    if verbose:
-        print("Predictions distribution:")
-        print(preds_distribution)
-        print("#" * 80)
-
-    correct = 0
-    for i in range(len(lower_case_preds)):
-        if lower_case_preds[i] == lower_case_true_labels[i]:
-            correct += 1
-
-    print(f'Final number of examples labeled "successfully": {correct}')
-    print(f"Accuracy: {correct / len(lower_case_preds)}")
-    print("#" * 80)
-
 class Oracle:
+    CHUNK_SIZE = 5
+
     def __init__(self, config: str, debug: bool = False) -> None:
         self.debug = debug
         self.config = Config.from_json(config)
@@ -50,33 +22,29 @@ class Oracle:
         self,
         dataset: str,
         input_column: str,
-        output_column: str,
-        output_dataset: str = "output.csv",
-        ground_truth_column=None,
-        n_trials: int = 1,
+        ground_truth_column: str = None,
+        delimiter: str = ',',
         max_items: int = 100,
     ) -> None:
-        dat = pd.read_csv(dataset)
+        dat = pd.read_csv(dataset, sep=delimiter)
+        max_items = min(max_items, len(dat))
 
-        input = dat[input_column].tolist()
-        truth = None if not ground_truth_column else dat[ground_truth_column].tolist()
+        input = dat[:max_items][input_column].tolist()
+        truth = None if not ground_truth_column else dat[:max_items][ground_truth_column].tolist()
 
-        yes_or_no = []
         llm_labels = []
         prompt_list = []
         total_tokens = 0
 
-        max_items = min(max_items, len(input))
-        num_sections = max(max_items / CHUNK_SIZE, 1)
-        for chunk_id, chunk in enumerate(
-            np.array_split(input[:max_items], num_sections)
-        ):
+        num_sections = max(max_items / self.CHUNK_SIZE, 1)
+        for chunk_id, chunk in enumerate(np.array_split(input, num_sections)):
             final_prompts = []
             for i, input_i in enumerate(chunk):
                 if (i + 1) % 10 == 0:
                     print(f"{i+1}/{len(input)}...")
 
                 # Fetch few-shot seed examples
+                # In the future this task will be delegated to an example selector
                 examples = self.config["seed_examples"]
 
                 # Construct Prompt to pass to LLM
@@ -92,20 +60,20 @@ class Oracle:
             response = self.llm.generate(prompt_list)
             for prompt, response_item in zip(final_prompts, response.generations):
                 generation = response_item[0]
-                llm_annotation: LLMAnnotation = self.task.parse_llm_response(
-                    prompt, generation)
-                yes_or_no.append(llm_annotation.successfully_labeled)
-                llm_labels.append(llm_annotation.label)
+                llm_labels.append(self.task.parse_llm_response(prompt, generation))
 
         # if true labels are provided, evaluate accuracy of predictions
         if truth:
-            evaluate(true_labels=truth, pred_labels=llm_labels, verbose=self.debug)
+            eval_result = self.task.eval(llm_labels, truth)
+            # TODO: serialize and write to file
+            for m in eval_result:
+                print(f"Metric: {m.name}: {m.value}")
 
         # Write output to CSV
-        if len(llm_labels) < len(dat):
-            llm_labels = llm_labels + [None for i in range(len(dat) - len(llm_labels))]
-        dat[output_column] = llm_labels
-        dat.to_csv(output_dataset)
+        output_df = dat[:max_items].copy()
+        output_df["llm_labeled_successfully"] = [l.successfully_labeled for l in llm_labels]
+        output_df["llm_label"] = [l.label for l in llm_labels]
+        output_df.to_csv(f"{dataset}_labeled.csv", sep=delimiter, header=True, index=False)
 
     def plan(
         self,
@@ -117,7 +85,7 @@ class Oracle:
         prompt_list = []
         total_tokens = 0
 
-        num_sections = max(len(input) / CHUNK_SIZE, 1)
+        num_sections = max(len(input) / self.CHUNK_SIZE, 1)
         for chunk_id, chunk in enumerate(
             np.array_split(input[: len(input)], num_sections)
         ):
