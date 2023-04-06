990707b4a3c2a61fae71342dc3c512fbc6b34760
Rishabh Bhargava <rishabh.bhargava93@gmail.com>
2023-04-05 17:58:50 -0700

    Minor cleanup to prompt for classification (#19)

--- refuel_oracle/tasks/classification.py
+++ refuel_oracle/tasks/classification.py
@@ -4,35 +4,34 @@ from typing import List
 from langchain.prompts.prompt import PromptTemplate
 from langchain.schema import Generation
 from loguru import logger
+from refuel_oracle.config import Config
+from refuel_oracle.schema import LLMAnnotation, Metric, MetricResult
+from refuel_oracle.tasks import BaseTask
 from sklearn.metrics import accuracy_score
 
-from refuel_oracle.config import Config
-from refuel_oracle.schema import Metric, MetricResult, LLMAnnotation
-from refuel_oracle.tasks import BaseTask
 
 class ClassificationTask(BaseTask):
 
-    DEFAULT_TASK_PROMPT = "Your job is to correctly label the provided input example into one of the following {num_labels} categories.\nCategories:{labels_list}\n"
-    DEFAULT_OUTPUT_FORMAT_PROMPT = "You will return the answer in JSON format with two keys: {\"answered\": \"can you answere this question. say YES or NO\", \"label\": \"the correct label\"}"
+    DEFAULT_TASK_PROMPT = "Your job is to correctly label the provided input example into one of the following {num_labels} categories.\nCategories:\n{labels_list}\n"
+    DEFAULT_OUTPUT_FORMAT_PROMPT = 'You will return the answer in JSON format with two keys: {"answered": "can you answer this question. say YES or NO", "label": "the correct label"}'
     PROMPT_TEMPLATE = "{prefix_prompt}\n{task_prompt}\n\n{output_prompt}\n\nSome examples with their output answers are provided below:\n{seed_examples}\n{current_example}"
-    PROMPT_TEMPLATE_VARIABLES = ["prefix_prompt",
-                                 "task_prompt",
-                                 "output_prompt",
-                                 "seed_examples",
-                                 "current_example"]
+    PROMPT_TEMPLATE_VARIABLES = [
+        "prefix_prompt",
+        "task_prompt",
+        "output_prompt",
+        "seed_examples",
+        "current_example",
+    ]
     EXAMPLE_PROMPT_TEMPLATE = "Example: {example}\nOutput: {output}\n"
     EXAMPLE_PROMPT_VARIABLES = ["example", "output"]
-    
+
     def __init__(self, config: Config) -> None:
         super().__init__(config)
-    
+
     def _to_default_output_format(self, label: str) -> str:
-        output = {
-            "answered": "yes",
-            "label": label
-        }
+        output = {"answered": "yes", "label": label}
         return json.dumps(output)
-    
+
     def _parse_default_output_format(self, output: str) -> dict:
         return json.loads(output.strip())
 
@@ -46,13 +45,13 @@ class ClassificationTask(BaseTask):
         task_prompt = self.config.get("task_prompt")
         if not task_prompt:
             task_prompt = self.DEFAULT_TASK_PROMPT.format(
-                num_labels=num_labels,
-                labels_list="\n".join(labels_list)
+                num_labels=num_labels, labels_list="\n".join(labels_list)
             )
-        
+
         # output format instructions
         output_prompt = self.config.get(
-            "output_format_prompt", self.DEFAULT_OUTPUT_FORMAT_PROMPT)
+            "output_format_prompt", self.DEFAULT_OUTPUT_FORMAT_PROMPT
+        )
 
         pt = PromptTemplate(
             input_variables=self.PROMPT_TEMPLATE_VARIABLES,
@@ -61,14 +60,14 @@ class ClassificationTask(BaseTask):
         return pt.partial(
             prefix_prompt=prefix_prompt,
             task_prompt=task_prompt,
-            output_prompt=output_prompt
+            output_prompt=output_prompt,
         )
 
     def construct_prompt(self, input: str, examples: List) -> str:
         # populate seed examples in the prompt
         example_prompt = PromptTemplate(
             input_variables=self.EXAMPLE_PROMPT_VARIABLES,
-            template=self.EXAMPLE_PROMPT_TEMPLATE
+            template=self.EXAMPLE_PROMPT_TEMPLATE,
         )
         formatted_examples = []
         for eg in examples:
@@ -81,17 +80,14 @@ class ClassificationTask(BaseTask):
             formatted_examples.append(
                 example_prompt.format(example=eg["example"], output=expected_output)
             )
-        
+
         # populate the current example in the prompt
-        current_example = example_prompt.format(
-            example=input, output=""
-        )
+        current_example = example_prompt.format(example=input, output="")
 
         return self.prompt_template.format(
-            seed_examples="\n".join(formatted_examples),
-            current_example=current_example
+            seed_examples="\n".join(formatted_examples), current_example=current_example
         )
-        
+
     def parse_llm_response(self, prompt: str, response: Generation) -> LLMAnnotation:
         # if the user specified an explicit output format prompt, don't postprocess the result
         # we will change this to allow a UDF to do this postprocessing
@@ -104,17 +100,21 @@ class ClassificationTask(BaseTask):
                 successfully_labeled = output.get("answered", False)
                 llm_label = output.get("label", "")
             except Exception as e:
-                logger.error(f"Error parsing LLM response: {response.text}. Encountered exception: {e}")
+                logger.error(
+                    f"Error parsing LLM response: {response.text}. Encountered exception: {e}"
+                )
 
         # TODO: parse generation info correctly to fetch & transform logprobs -> score
         return LLMAnnotation(
             prompt=prompt,
             successfully_labeled=successfully_labeled,
             label=llm_label,
-            generation_info=response.generation_info
+            generation_info=response.generation_info,
         )
-    
-    def eval(self, llm_labels: List[LLMAnnotation], gt_labels: List[str]) -> List[MetricResult]:
+
+    def eval(
+        self, llm_labels: List[LLMAnnotation], gt_labels: List[str]
+    ) -> List[MetricResult]:
         """Evaluate the LLM generated labels by comparing them against ground truth
 
         Args:
@@ -129,20 +129,25 @@ class ClassificationTask(BaseTask):
         # support
         support = len(gt_labels)
         eval_metrics.append(
-            MetricResult(metric_type=Metric.SUPPORT, name="support", value=support))
+            MetricResult(metric_type=Metric.SUPPORT, name="support", value=support)
+        )
 
         # completion rate
         num_labeled = sum([l.successfully_labeled for l in llm_labels])
         fraction_completed = round(num_labeled * 1.0 / support, 2)
         eval_metrics.append(
-            MetricResult(metric_type=Metric.COMPLETION_RATE, name="completion_rate", value=fraction_completed))
-        
+            MetricResult(
+                metric_type=Metric.COMPLETION_RATE,
+                name="completion_rate",
+                value=fraction_completed,
+            )
+        )
+
         # accuracy
         pred_labels = [l.label for l in llm_labels]
         accuracy = accuracy_score(gt_labels, pred_labels)
         eval_metrics.append(
-            MetricResult(metric_type=Metric.ACCURACY, name="accuracy", value=accuracy))
-        
+            MetricResult(metric_type=Metric.ACCURACY, name="accuracy", value=accuracy)
+        )
+
         return eval_metrics
-
-
