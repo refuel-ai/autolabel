b70aac2e5ed501b31b2a1ca7415c4cd26d80b4e0
Nihit <nihit@cs.stanford.edu>
2023-04-07 11:17:09 -0700

    Add support for multi-column inputs (#24)
    * Add support for multi-column inputs
    
    * Add multi-col example for CC transactions

--- refuel_oracle/oracle.py
+++ refuel_oracle/oracle.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pandas as pd
 import numpy as np
 
@@ -11,37 +12,60 @@ from refuel_oracle.utils import (
 
 class Oracle:
     CHUNK_SIZE = 5
+    DEFAULT_SEPARATOR = ","
 
     def __init__(self, config: str, debug: bool = False) -> None:
         self.debug = debug
         self.config = Config.from_json(config)
         self.llm = LLMFactory.from_config(self.config)
         self.task = TaskFactory.from_config(self.config)
+    
+    # TODO: all this will move to a separate input parser class
+    # this is a temporary solution to quickly add this feature and unblock expts
+    def _read_csv(self, csv_file: str, max_items: int = None) -> Tuple:
+        dataset_schema = self.config.get("dataset_schema", {})
+        delimiter = dataset_schema.get("delimiter", self.DEFAULT_SEPARATOR)
+        input_columns = dataset_schema.get("input_columns", [])
+        input_template = dataset_schema.get("input_template")
+        label_column = dataset_schema.get("label_column")
+        
+        dat = pd.read_csv(csv_file, sep=delimiter)
+        if max_items and max_items > 0:
+            max_items = min(max_items, len(dat))
+            dat = dat[:max_items]
+        
+        # construct input row
+        if input_template:
+            # User has explicitly passed in a template to stitch together input columns. use this directly
+            inputs = dat[input_columns].apply(
+                lambda row: input_template.format(**row), axis=1).tolist()
+        else:
+            # use a default format
+            inputs = dat[input_columns].apply(
+                lambda row: "; ".join([str(v) for (k, v) in row.items()]), axis=1).tolist()
+
+        gt_labels = None if not label_column else dat[label_column].tolist()
+        return (dat, inputs, gt_labels)
+
 
     def annotate(
         self,
         dataset: str,
-        input_column: str,
-        ground_truth_column: str = None,
-        delimiter: str = ',',
         max_items: int = 100,
     ) -> None:
-        dat = pd.read_csv(dataset, sep=delimiter)
-        max_items = min(max_items, len(dat))
-
-        input = dat[:max_items][input_column].tolist()
-        truth = None if not ground_truth_column else dat[:max_items][ground_truth_column].tolist()
-
+        
+        df, inputs, gt_labels = self._read_csv(dataset, max_items)
+        
         llm_labels = []
         prompt_list = []
         total_tokens = 0
 
         num_sections = max(max_items / self.CHUNK_SIZE, 1)
-        for chunk_id, chunk in enumerate(np.array_split(input, num_sections)):
+        for chunk_id, chunk in enumerate(np.array_split(inputs, num_sections)):
             final_prompts = []
             for i, input_i in enumerate(chunk):
                 if (i + 1) % 10 == 0:
-                    print(f"{i+1}/{len(input)}...")
+                    print(f"{i+1}/{len(inputs)}...")
 
                 # Fetch few-shot seed examples
                 # In the future this task will be delegated to an example selector
@@ -63,35 +87,33 @@ class Oracle:
                 llm_labels.append(self.task.parse_llm_response(generation))
 
         # if true labels are provided, evaluate accuracy of predictions
-        if truth:
-            eval_result = self.task.eval(llm_labels, truth)
+        if gt_labels:
+            eval_result = self.task.eval(llm_labels, gt_labels)
             # TODO: serialize and write to file
             for m in eval_result:
                 print(f"Metric: {m.name}: {m.value}")
 
         # Write output to CSV
-        output_df = dat[:max_items].copy()
+        output_df = df.copy()
         output_df["llm_labeled_successfully"] = [l.successfully_labeled for l in llm_labels]
         output_df["llm_label"] = [l.label for l in llm_labels]
-        output_df.to_csv(f"{dataset}_labeled.csv", sep=delimiter, header=True, index=False)
+        output_df.to_csv(f"{dataset}_labeled.csv", sep=self.DEFAULT_SEPARATOR, header=True, index=False)
 
     def plan(
         self,
         dataset: str,
-        input_column: str,
     ):
-        dat = pd.read_csv(dataset)
-        input = dat[input_column].tolist()
+        _, inputs, _ = self._read_csv(dataset)
         prompt_list = []
         total_tokens = 0
 
-        num_sections = max(len(input) / self.CHUNK_SIZE, 1)
+        num_sections = max(len(inputs) / self.CHUNK_SIZE, 1)
         for chunk_id, chunk in enumerate(
-            np.array_split(input[: len(input)], num_sections)
+            np.array_split(inputs[: len(inputs)], num_sections)
         ):
             for i, input_i in enumerate(chunk):
                 if (i + 1) % 10 == 0:
-                    print(f"{i+1}/{len(input)}...")
+                    print(f"{i+1}/{len(inputs)}...")
                 # Fetch few-shot seed examples
                 examples = self.config["seed_examples"]
 
@@ -103,8 +125,8 @@ class Oracle:
                 prompt_list.append(final_prompt)
         total_cost = calculate_cost(self.config, total_tokens)
         print(f"Total Estimated Cost: {total_cost}")
-        print(f"Number of examples to label: {len(input)}")
-        print(f"Average cost per example: {total_cost/len(input)}")
+        print(f"Number of examples to label: {len(inputs)}")
+        print(f"Average cost per example: {total_cost/len(inputs)}")
         print(f"\n\nFinal prompt example:\n\n{prompt_list[0]}")
         return
 
