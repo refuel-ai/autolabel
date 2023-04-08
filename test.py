fbd61a2155a4f5742869c0589134e25c281bccf7
Rishabh Bhargava <rishabh.bhargava93@gmail.com>
2023-04-07 23:40:09 -0400

    Cleaning up the test file

--- test.py
+++ test.py
@@ -10,25 +10,10 @@ ag_news_file_name = "examples/ag_news_filtered_labels_sampled.csv"
 annotator = Oracle(config_path, debug=True)
 
 print("Running Oracle.plan() to calculate expected cost of labeling dataset.")
-annotator.plan(
-    dataset=os.path.join(curr_directory, ag_news_file_name),
-    input_column="description",
-)
+annotator.plan(dataset=os.path.join(curr_directory, ag_news_file_name))
 
 print("Running Oracle.annotate() on a subset of items in dataset")
-annotation_obj = annotator.annotate(
+annotator.annotate(
     dataset=os.path.join(curr_directory, ag_news_file_name),
-    input_column="description",
-    ground_truth_column="label",
-    output_column="llm_labels",
-    max_items=10,
+    max_items=15,
 )
-
-for i in range(5):
-    print(f"\n\n#### output[{i}] ####")
-    print("Data:")
-    print(annotation_obj.data[i])
-    print("Label:")
-    print(annotation_obj.labels[i])
-    print("Confidence:")
-    print(annotation_obj.confidence[i])
