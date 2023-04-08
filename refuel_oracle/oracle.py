079abe7629ee525f314110b7369c6a6d97a27c8b
Patrick Cramer <pcramer247@gmail.com>
2023-04-08 14:53:00 -0500

    log into window size is 50 instead of 5 now

--- refuel_oracle/oracle.py
+++ refuel_oracle/oracle.py
@@ -67,7 +67,7 @@ class Oracle:
         for chunk_id, chunk in enumerate(np.array_split(inputs, num_sections)):
             if chunk_id % 10 == 0:
                 print(
-                    f"Labeling {chunk_id*self.CHUNK_SIZE}-{chunk_id*self.CHUNK_SIZE+self.CHUNK_SIZE}"
+                    f"Labeling {chunk_id*self.CHUNK_SIZE}-{chunk_id*self.CHUNK_SIZE+(self.CHUNK_SIZE*10-1)}"
                 )
             final_prompts = []
             for i, input_i in enumerate(chunk):
