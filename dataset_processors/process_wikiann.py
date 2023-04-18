e78d693b2ba8120a6a9db036fa9d9d58170b3ab4
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2023-04-18 11:50:13 -0700

    WikiANN dataset processor

---
+++ dataset_processors/process_wikiann.py
@@ -0,0 +1,25 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+import json
+
+dataset_csv = open("wikiann.csv", "w")
+dataset_csv.write("Text%IndividualLabels%CategorizedLabels")
+dataset = load_dataset("wikiann", "en")
+entity_category_mapping = {"LOC": "Location", "ORG": "Organization", "PER": "Person"}
+for item in dataset["test"]:
+    curr_text = " ".join(item["tokens"])
+    curr_labels = item["spans"]
+    individual_labels = []
+    individual_entity_categories = {"Location": [], "Organization": [], "Person": []}
+
+    for label in curr_labels:
+        entity_category = entity_category_mapping[label.split(":")[0]]
+        entity_text = ":".join(label.split(":")[1:]).strip(" ").strip("\n")
+        individual_entity_categories[entity_category].append(entity_text)
+        individual_labels.append({"Description": entity_category, "Text": entity_text})
+
+    dataset_csv.write(
+        f"{curr_text}%{json.dumps(individual_labels)}%{json.dumps(individual_entity_categories)}\n"
+    )
+
+dataset_csv.close()
