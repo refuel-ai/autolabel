fce6cfd4d553f5d55d4780b1f4771c205bc87343
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2023-04-24 23:23:23 -0700

    WikiANN 500 labels with davinci

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from refuel_oracle.oracle import Oracle
 
 curr_directory = os.path.dirname(os.path.abspath(__file__))
 
-config_path = os.path.join(curr_directory, "examples/config_imdb_oai.json")
-data_file_name = "examples/filtered_imdb.csv"
+# config_path = os.path.join(curr_directory, "examples/config_imdb_oai.json")
+# data_file_name = "examples/filtered_imdb.csv"
 
 # config_path = os.path.join(curr_directory, "examples/config_medqa_oai.json")
 # data_file_name = "examples/filtered_medqa.csv"
@@ -16,6 +16,9 @@ data_file_name = "examples/filtered_imdb.csv"
 # config_path = os.path.join(curr_directory, "examples/config_sciq_oai.json")
 # data_file_name = "examples/filtered_sciq.csv"
 
+config_path = os.path.join(curr_directory, "examples/config_wikiann.json")
+data_file_name = "examples/wikiann.csv"
+
 annotator = Oracle(config_path, debug=True)
 plan_first = False
 if plan_first:
@@ -25,5 +28,5 @@ if plan_first:
 print("Running Oracle.annotate() on a subset of items in dataset")
 annotator.annotate(
     dataset=os.path.join(curr_directory, data_file_name),
-    max_items=100,
+    max_items=500,
 )
