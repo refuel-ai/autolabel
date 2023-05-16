4bdb620a6094172c4e1559950a1ce6b28669e7b5
Abhinav Naikawadi <abhinavn@Abhinavs-MacBook-Pro.local>
2023-05-15 17:19:58 -0700

    entity_matching.py

--- src/autolabel/tasks/entity_matching.py
+++ src/autolabel/tasks/entity_matching.py
@@ -1,10 +1,10 @@
-from typing import List, Dict, Tuple
+from typing import Dict, List, Tuple
 
-from langchain.prompts.prompt import PromptTemplate
 from autolabel.confidence import ConfidenceCalculator
 from autolabel.configs import TaskConfig
 from autolabel.schema import LLMAnnotation, Metric, MetricResult
 from autolabel.tasks import BaseTask
+from langchain.prompts.prompt import PromptTemplate
 from sklearn.metrics import accuracy_score
 
 
@@ -12,19 +12,7 @@ class EntityMatchingTask(BaseTask):
     JSON_OUTPUT_FORMAT_PROMPT = 'You will return the answer in JSON format with one key: {"label": "duplicate or not duplicate"}\n'
     CSV_OUTPUT_FORMAT_PROMPT = 'You will return the answer in CSV format with one element: "duplicate or not duplicate"\n'
 
-    task_prompt = "Your job is to tell if the two given entities are duplicates or not. Say duplicate, if they are duplicate and not duplicate otherwise. Options:\nduplicate\nnot duplicate\n"
-    example_prompt_template = (
-        "Entity1: {entity1}\nEntity2: {entity2}\nAnswer:{answer}\n"
-    )
-    example_prompt_variables = ["entity1", "entity2", "answer"]
-
-    explanation_generation_prompt = "{prefix_prompt}\n You will be given two entities. Your job is to provide an explanation for why the two entities are duplicates or not duplicates. Think step by step and generate an explanation. The last line of the explanation should be - So, the answer is <answer>.\nEntity1: {entity1}\nEntity2: {entity2}\nAnswer: {answer}\nExplanation: "
-    explanation_generation_prompt_variables = [
-        "prefix_prompt",
-        "entity1",
-        "entity2",
-        "answer",
-    ]
+    task_prompt = "Your job is to tell if the two given entities are duplicates or not. Say duplicate, if they are duplicate and not duplicate otherwise.\nOptions:\nduplicate\nnot duplicate\n"
 
     def __init__(self, config: TaskConfig) -> None:
         super().__init__(config)
@@ -42,28 +30,15 @@ class EntityMatchingTask(BaseTask):
         )
 
     def construct_prompt(self, input: Dict, examples: List[Dict]) -> str:
-        # populate seed examples in the prompt
-        example_prompt = PromptTemplate(
-            input_variables=self.example_prompt_variables,
-            template=self.example_prompt_template,
-        )
-        formatted_examples = []
-        for eg in examples:
-            expected_output = self._to_output_format(eg["label"])
-            formatted_examples.append(
-                example_prompt.format(
-                    entity1=eg["entity1"],
-                    entity2=eg["entity2"],
-                    answer=expected_output,
-                )
-            )
+        example_prompt_template = self.dataset_config.get_example_prompt_template()
+        example_label_template = self.dataset_config.get_example_label_template()
+        example_template = example_prompt_template + "\n" + example_label_template
 
-        # populate the current example in the prompt
-        current_example = example_prompt.format(
-            entity1=input["entity1"],
-            entity2=input["entity2"],
-            answer="",  # we don't know the answer yet
+        formatted_examples = list(
+            map(lambda example: example_template.format(**example), examples)
         )
+
+        current_example = example_prompt_template.format(**input)
 
         if len(examples):
             seed_examples_prompt = self.seed_examples_prompt
@@ -76,18 +51,6 @@ class EntityMatchingTask(BaseTask):
             seed_examples_prompt=seed_examples_prompt,
         )
         return prompt
-
-    def generate_explanation(self, example: Dict) -> str:
-        example_prompt = PromptTemplate(
-            input_variables=self.explanation_generation_prompt_variables,
-            template=self.explanation_generation_prompt,
-        )
-        return example_prompt.format(
-            prefix_prompt=self.prefix_prompt,
-            entity1=example["entity1"],
-            entity2=example["entity2"],
-            answer=example["label"],
-        )
 
     def auroc_score_labels(
         self, gt_labels, llm_labels
