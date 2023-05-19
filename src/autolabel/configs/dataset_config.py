25cef067c4e82e2ac7d4cdc3f2775059be5ef407
Nihit <nihit@cs.stanford.edu>
2023-05-18 21:45:58 -0700

    remove input_columns; flatten dataset schema config

--- src/autolabel/configs/dataset_config.py
+++ src/autolabel/configs/dataset_config.py
@@ -16,7 +16,6 @@ class DatasetConfig(BaseConfig):
     INPUT_COLUMNS_KEY = "input_columns"
     LABEL_COLUMN_KEY = "label_column"
     SEED_EXAMPLES_KEY = "seed_examples"
-    DATASET_SCHEMA_KEY = "dataset_schema"
     DELIMITER_KEY = "delimiter"
     LABELS_LIST_KEY = "labels_list"
     EMPTY_RESPONSE_KEY = "empty_response"
@@ -26,9 +25,6 @@ class DatasetConfig(BaseConfig):
 
     def __init__(self, config: Union[str, Dict]) -> None:
         super().__init__(config)
-
-    def get_input_columns(self) -> List:
-        return self.config[self.DATASET_SCHEMA_KEY][self.INPUT_COLUMNS_KEY]
 
     def get_label_column(self) -> str:
         """
@@ -43,9 +39,10 @@ class DatasetConfig(BaseConfig):
         return self.config.get(self.SEED_EXAMPLES_KEY, [])
 
     def get_delimiter(self) -> str:
-        return self.config[self.DATASET_SCHEMA_KEY].get(
-            self.DELIMITER_KEY, self.DEFAULT_SEPARATOR
-        )
+        """
+        Returns the string used to seperate values in dataset. Defaults to ','
+        """
+        return self.config.get(self.DELIMITER_KEY, self.DEFAULT_SEPARATOR)
 
     def get_example_template(self) -> str:
         """
