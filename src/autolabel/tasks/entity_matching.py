aa020338fb3286ffe00975d73418cb24d6633979
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2023-05-26 11:27:39 -0700

    Computing metrics only if labels are available

--- src/autolabel/tasks/entity_matching.py
+++ src/autolabel/tasks/entity_matching.py
@@ -137,15 +137,19 @@ class EntityMatchingTask(BaseTask):
             ) = self.get_labels_predictions_with_threshold(
                 gt_labels, llm_labels, threshold
             )
-            eval_metrics_map[Metric.SUPPORT].append(
-                (len(curr_gt_labels), f"index={index}")
-            )
-            eval_metrics_map[Metric.COMPLETION_RATE].append(
-                (len(curr_gt_labels) / float(len(gt_labels)), f"index={index}")
-            )
-            eval_metrics_map[Metric.ACCURACY].append(
-                (accuracy_score(curr_gt_labels, curr_llm_labels), f"index={index}")
-            )
+
+            if len(gt_labels) > 0:
+                eval_metrics_map[Metric.COMPLETION_RATE].append(
+                    (len(curr_gt_labels) / float(len(gt_labels)), f"index={index}")
+                )
+
+            if len(curr_gt_labels) > 0:
+                eval_metrics_map[Metric.SUPPORT].append(
+                    (len(curr_gt_labels), f"index={index}")
+                )
+                eval_metrics_map[Metric.ACCURACY].append(
+                    (accuracy_score(curr_gt_labels, curr_llm_labels), f"index={index}")
+                )
             eval_metrics_map[Metric.THRESHOLD].append((threshold, f"index={index}"))
 
         eval_metrics.extend(
