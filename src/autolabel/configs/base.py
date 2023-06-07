9c006484d73c96ada64719ae6fd02b9c9f25b36d
Patrick Cramer <pcramer247@gmail.com>
2023-06-07 14:29:24 -0500

    agent -> LabelingAgent

--- src/autolabel/configs/base.py
+++ src/autolabel/configs/base.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 
 class BaseConfig:
-    """Used for parsing, validating, and storing information about the labeling task passed to the agent. Additional config classes should extend from this base class."""
+    """Used for parsing, validating, and storing information about the labeling task passed to the LabelingAgent. Additional config classes should extend from this base class."""
 
     def __init__(self, config: Union[str, Dict]) -> None:
         if isinstance(config, str):
