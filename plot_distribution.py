671123d3801806056129cfafb0a2ba0cb84c6ab7
Dhruva Bansal <bdhruva@stanford.edu>
2023-06-11 11:10:38 -0700

    Documentation for confidence api (#227)
    * Documentation for confidence api
    
    * Addressing comments, adding examples

--- plot_distribution.py
+++
@@ -1,8 +0,0 @@
-import pickle as pkl
-
-from autolabel.confidence import ConfidenceCalculator
-
-ConfidenceCalculator.plot_data_distribution(
-    match=pkl.load(open("matches.pkl", "rb")),
-    confidence=pkl.load(open("confidences.pkl", "rb")),
-)
