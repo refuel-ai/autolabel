3128ead2f2a4d12f05c67614673f5d5cdc208628
Abhinav Naikawadi <abhinavn@Abhinavs-MacBook-Pro.local>
2023-06-13 14:00:07 -0700

    dummy test for mocking function

--- tests/unit/dummy_test.py
+++ tests/unit/dummy_test.py
@@ -1,6 +1,14 @@
 from autolabel import LabelingAgent
+from langchain.llms.fake import FakeListLLM
 
 
-def test_labeling_agent_chunk_size():
+def test_labeling_agent_config():
     agent = LabelingAgent(config="examples/banking/config_banking.json")
     assert agent.config != None
+
+
+def test_mock_example(mocker):
+    agent = LabelingAgent(config="examples/banking/config_banking.json")
+    mocker.patch("autolabel.labeler.LabelingAgent.run", return_value=True)
+    res = agent.run("examples/banking/test.csv")
+    assert res == True
