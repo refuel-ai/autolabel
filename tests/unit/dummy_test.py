8e7c6727782c8dbe045be5e8e0f31cf0ed78d802
Abhinav Naikawadi <abhinavn@Abhinavs-MacBook-Pro.local>
2023-06-13 14:38:39 -0700

    add assets folder

--- tests/unit/dummy_test.py
+++ tests/unit/dummy_test.py
@@ -2,12 +2,12 @@ from autolabel import LabelingAgent
 
 
 def test_labeling_agent_config():
-    agent = LabelingAgent(config="examples/banking/config_banking.json")
+    agent = LabelingAgent(config="assets/testing/config_banking.json")
     assert agent.config != None
 
 
 def test_mock_example(mocker):
-    agent = LabelingAgent(config="examples/banking/config_banking.json")
+    agent = LabelingAgent(config="assets/testing/config_banking.json")
     mocker.patch("autolabel.labeler.LabelingAgent.run", return_value=True)
-    res = agent.run("examples/banking/test.csv")
+    res = agent.run("assets/testing/banking_test.csv")
     assert res == True
