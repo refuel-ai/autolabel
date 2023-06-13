a57654cc5dca3a362d921cd7dad1546ecf96a42a
Abhinav N <114095553+Abhinav-Naikawadi@users.noreply.github.com>
2023-06-13 14:42:59 -0700

    Dummy example test for mocking function (#287)
    * dummy test for mocking function
    
    * remove unused import
    
    * pytest-mock
    
    * add assets folder
    
    ---------
    
    Co-authored-by: Abhinav Naikawadi <abhinavn@Abhinavs-MacBook-Pro.local>

--- tests/unit/dummy_test.py
+++ tests/unit/dummy_test.py
@@ -1,6 +1,13 @@
 from autolabel import LabelingAgent
 
 
-def test_labeling_agent_chunk_size():
-    agent = LabelingAgent(config="examples/banking/config_banking.json")
+def test_labeling_agent_config():
+    agent = LabelingAgent(config="assets/testing/config_banking.json")
     assert agent.config != None
+
+
+def test_mock_example(mocker):
+    agent = LabelingAgent(config="assets/testing/config_banking.json")
+    mocker.patch("autolabel.labeler.LabelingAgent.run", return_value=True)
+    res = agent.run("assets/testing/banking_test.csv")
+    assert res == True
