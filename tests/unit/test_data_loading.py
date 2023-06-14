90868414a23113a27fedf38c5b544b372396d3d1
iomap <67727644+iomap@users.noreply.github.com>
2023-06-14 12:07:11 -0500

    Add tests for 1) data_loading and 2) config validation. Also add testing assets directory (#292)
    * add tests for data loading functions
    
    * add test for load_jsonl
    
    * add test for read_file
    
    * add assets for testing data loading
    
    * fix test_read_file to use read_file instead of read_csv/jsonl
    
    * add more testing assets and initial commit for test_config.py
    
    * fix filepaths to start from base directory
    
    * move tests into unit/ directory
    
    * remove return True/False from tests. Also remove print statements
    
    * remove test for unrecognized file types
    
    * trim down test assets
    
    * imporove coverage of config test

---
+++ tests/unit/test_data_loading.py
@@ -0,0 +1,70 @@
+from autolabel import LabelingAgent
+from autolabel.dataset_loader import DatasetLoader
+from pandas import DataFrame
+
+csv_path = "tests/assets/banking/test.csv"
+jsonl_path = "tests/assets/banking/test.jsonl"
+config_path = "tests/assets/banking/config_banking.json"
+
+
+def test_read_csv():
+    agent = LabelingAgent(config=config_path)
+    data = DatasetLoader.read_csv(csv_path, agent.config)
+    # test return types
+    assert isinstance(data, tuple)
+    assert isinstance(data[0], DataFrame)
+    assert isinstance(data[1], list)
+    assert isinstance(data[2], list) or data[2] is None
+    # test reading_csv with max_items = 5, start_index = 5
+    data_max_5_index_5 = DatasetLoader.read_csv(
+        csv_path, agent.config, max_items=5, start_index=5
+    )
+    assert data_max_5_index_5[0].shape[0] == 5
+    assert data_max_5_index_5[0].iloc[0].equals(data[0].iloc[5])
+    assert len(data_max_5_index_5[1]) == 5
+    assert len(data_max_5_index_5[2]) == 5
+
+
+def test_read_dataframe():
+    agent = LabelingAgent(config=config_path)
+    df, _, _ = DatasetLoader.read_csv(csv_path, agent.config)
+    data = DatasetLoader.read_dataframe(df, agent.config)
+    # test return types
+    assert isinstance(data, tuple)
+    assert isinstance(data[0], DataFrame)
+    assert isinstance(data[1], list)
+    assert isinstance(data[2], list) or data[2] is None
+    # confirm data matches
+    assert df.equals(data[0])
+    # test loading data with max_items = 5, start_index = 5
+    data_max_5_index_5 = DatasetLoader.read_dataframe(
+        df, agent.config, max_items=5, start_index=5
+    )
+    assert data_max_5_index_5[0].shape[0] == 5
+    assert data_max_5_index_5[0].iloc[0].equals(data[0].iloc[5])
+    assert len(data_max_5_index_5[1]) == 5
+    assert len(data_max_5_index_5[2]) == 5
+
+
+def test_read_jsonl():
+    agent = LabelingAgent(config=config_path)
+    data = DatasetLoader.read_jsonl(jsonl_path, agent.config)
+    # test return types
+    assert isinstance(data, tuple)
+    assert isinstance(data[0], DataFrame)
+    assert isinstance(data[1], list)
+    assert isinstance(data[2], list) or data[2] is None
+    # test reading_csv with max_items = 5, start_index = 5
+    data_max_5_index_5 = DatasetLoader.read_jsonl(
+        jsonl_path, agent.config, max_items=5, start_index=5
+    )
+    assert data_max_5_index_5[0].shape[0] == 5
+    assert data_max_5_index_5[0].iloc[0].equals(data[0].iloc[5])
+    assert len(data_max_5_index_5[1]) == 5
+    assert len(data_max_5_index_5[2]) == 5
+
+
+def test_read_file():
+    agent = LabelingAgent(config=config_path)
+    csv_data, _, _ = DatasetLoader.read_file(csv_path, agent.config)
+    jsonl_data, _, _ = DatasetLoader.read_file(jsonl_path, agent.config)
