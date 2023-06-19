e66a15199a393518be90b076735b1f5333005f86
Nihit <nihit@cs.stanford.edu>
2023-06-19 16:31:47 -0700

    add support for all gpt-3.5 and gpt-4 model versions

--- src/autolabel/models/openai.py
+++ src/autolabel/models/openai.py
@@ -16,7 +16,11 @@ class OpenAILLM(BaseModel):
         "gpt-3.5-turbo",
         "gpt-3.5-turbo-0613",
         "gpt-3.5-turbo-16k",
+        "gpt-3.5-turbo-16k-0613",
         "gpt-4",
+        "gpt-4-0613",
+        "gpt-4-32k",
+        "gpt-4-32k-0613",
     ]
     MODELS_WITH_TOKEN_PROBS = ["text-curie-001", "text-davinci-003"]
 
@@ -39,7 +43,11 @@ class OpenAILLM(BaseModel):
         "gpt-3.5-turbo": 0.0015 / 1000,
         "gpt-3.5-turbo-0613": 0.0015 / 1000,
         "gpt-3.5-turbo-16k": 0.003 / 1000,
+        "gpt-3.5-turbo-16k-0613": 0.003 / 1000,
         "gpt-4": 0.03 / 1000,
+        "gpt-4-0613": 0.03 / 1000,
+        "gpt-4-32k": 0.06 / 1000,
+        "gpt-4-32k-0613": 0.06 / 1000,
     }
     COST_PER_COMPLETION_TOKEN = {
         "text-davinci-003": 0.02 / 1000,
@@ -47,7 +55,11 @@ class OpenAILLM(BaseModel):
         "gpt-3.5-turbo": 0.002 / 1000,
         "gpt-3.5-turbo-0613": 0.002 / 1000,
         "gpt-3.5-turbo-16k": 0.004 / 1000,
-        "gpt-4": 0.06 / 1000,  # $0.06 per 1000 tokens in response
+        "gpt-3.5-turbo-16k-0613": 0.004 / 1000,
+        "gpt-4": 0.06 / 1000,
+        "gpt-4-0613": 0.06 / 1000,
+        "gpt-4-32k": 0.12 / 1000,
+        "gpt-4-32k-0613": 0.12 / 1000,
     }
 
     @cached_property
