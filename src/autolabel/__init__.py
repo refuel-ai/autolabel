bc44c0c12732b5615089ac40fcf811cbeabb40de
Nihit <nihit@cs.stanford.edu>
2023-07-11 17:34:15 -0700

    version bump 0.0.7 -> 0.0.8 (#452)
    version bump

--- src/autolabel/__init__.py
+++ src/autolabel/__init__.py
@@ -1,2 +1,10 @@
+from importlib import metadata
+
 from .labeler import LabelingAgent
 from .utils import get_data
+
+try:
+    __version__ = metadata.version("refuel-autolabel")
+except metadata.PackageNotFoundError:
+    # If the package metadata is not available.
+    __version__ = ""
