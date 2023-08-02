e9dbcab3ab3c92dc11ef77812cc2964eacebb9c3
Tyler <terrifictyler8@gmail.com>
2023-08-02 11:49:25 -0700

    default non-ocr, moved imports to optional

--- src/autolabel/transforms/pdf.py
+++ src/autolabel/transforms/pdf.py
@@ -1,8 +1,5 @@
 from typing import List, Dict, Any
 
-from pdf2image import convert_from_path
-import pytesseract
-
 from autolabel.transforms import BaseTransform
 
 
@@ -11,12 +8,14 @@ class PDFTransform(BaseTransform):
         self,
         output_columns: List[str],
         file_path_column: str,
+        ocr_enabled: bool = False,
         page_header: str = "Page {page_num}: ",
         page_sep: str = "\n\n",
     ) -> None:
         """The output columns for this class should be in the order: [content_column, num_pages_column]"""
         super().__init__(output_columns)
         self.file_path_column = file_path_column
+        self.ocr_enabled = ocr_enabled
         self.page_header = page_header
         self.page_sep = page_sep
 
@@ -26,6 +25,26 @@ class PDFTransform(BaseTransform):
 
     @staticmethod
     def extract_text(path: str) -> List[str]:
+        """
+        This function extracts text from a PDF file using the pdfplumber library.
+
+        Args:
+            path (str): The path to the PDF file.
+
+        Returns:
+            List[str]: A list of strings, each index containing the extracted text from each page of the PDF file.
+        """
+        try:
+            from langchain.document_loaders import PDFPlumberLoader
+        except ImportError:
+            raise ImportError(
+                "pdfplumber is required to use the pdf transform. Please install pdfplumber with the following command: pip install pdfplumber"
+            )
+        loader = PDFPlumberLoader(path)
+        return [doc.page_content for doc in loader.load()]
+
+    @staticmethod
+    def extract_text_ocr(path: str) -> List[str]:
         """This function extracts text from a PDF file using the pdf2image and pytesseract libraries.
 
         Args:
@@ -34,12 +53,25 @@ class PDFTransform(BaseTransform):
         Returns:
             List[str]: A list of strings, one for each page of the PDF file.
         """
+        try:
+            from pdf2image import convert_from_path
+            import pytesseract
+            from pytesseract import TesseractNotFoundError
+        except ImportError:
+            raise ImportError(
+                "pdf2image and pytesseract are required to use the pdf transform with ocr. Please install pdf2image and pytesseract with the following command: pip install pdf2image pytesseract"
+            )
         pages = convert_from_path(path)
-        texts = []
-        for page in pages:
-            text = pytesseract.image_to_string(page)
-            texts.append(text)
-        return texts
+        try:
+            texts = []
+            for page in pages:
+                text = pytesseract.image_to_string(page)
+                texts.append(text)
+            return texts
+        except TesseractNotFoundError:
+            raise ImportError(
+                "The tesseract engine is required to use the pdf transform with ocr. Please see https://tesseract-ocr.github.io/tessdoc/Installation.html for installation instructions."
+            )
 
     def transform(self, row: Dict[str, any]) -> Dict[str, any]:
         """This function transforms a PDF file into a string of text. It uses the PyPDFLoader to load and split the PDF into pages.
@@ -51,13 +83,10 @@ class PDFTransform(BaseTransform):
         Returns:
             Dict[str, any]: The transformed row of data.
         """
-        try:
-            from langchain.document_loaders import PyPDFLoader
-        except ImportError:
-            raise ImportError(
-                "pypdf is required to use the pdf transform. Please install pypdf with the following command: pip install pypdf"
-            )
-        pages = self.extract_text(row[self.file_path_column])
+        if self.ocr_enabled:
+            pages = self.extract_text_ocr(row[self.file_path_column])
+        else:
+            pages = self.extract_text(row[self.file_path_column])
         page_contents = []
         for idx, page in enumerate(pages):
             page_contents.append(self.page_header.format(page_num=idx + 1) + page)
--- tests/unit/data_loaders/test_transform.py
+++ tests/unit/data_loaders/test_transform.py
@@ -1,46 +1,35 @@
 from autolabel.transforms.pdf import PDFTransform
 
 
-RESUME_PDF_CONTENT = """Page 1: Functional Resume Sample 
- 
-John W. Smith   
-2002 Front Range Way Fort Collins, CO 80525  
-jwsmith@colostate.edu  
- 
-Career Summary 
- 
-Four years experience in early childhood development with a di verse background in the care of 
-special needs children and adults.  
-  
-Adult Care Experience  
- 
-• Determined work placement for 150 special needs adult clients.  
-• Maintained client databases and records.  
-• Coordinated client contact with local health care professionals on a monthly basis.     
-• Managed 25 volunteer workers.     
- 
-Childcare Experience  
- 
-• Coordinated service assignments for 20 part -time counselors and 100 client families. 
-• Oversaw daily activity and outing planning for 100 clients.  
-• Assisted families of special needs clients with researching financial assistance and 
-healthcare. 
-• Assisted teachers with managing daily classroom activities.    
-• Oversaw daily and special st udent activities.     
- 
-Employment History  
- 1999-2002  Counseling Supervisor, The Wesley Ce nter, Little Rock, Arkansas.    
-1997-1999  Client Specialist, Rainbow Special Ca re Center, Little Rock, Arkansas  
-1996-1997 Teacher’s Assistant, Cowell Elem entary, Conway, Arkansas     
- 
-Education 
- 
-University of Arkansas at Little Rock, Little Rock, AR  
- 
-• BS in Early Childhood Development (1999) 
-• BA in Elementary Education (1998) 
-• GPA (4.0 Scale):  Early Childhood Developm ent – 3.8, Elementary Education – 3.5, 
-Overall 3.4.  
+RESUME_PDF_CONTENT = """Page 1: Functional Resume Sample
+John W. Smith
+2002 Front Range Way Fort Collins, CO 80525
+jwsmith@colostate.edu
+Career Summary
+Four years experience in early childhood development with a diverse background in the care of
+special needs children and adults.
+Adult Care Experience
+• Determined work placement for 150 special needs adult clients.
+• Maintained client databases and records.
+• Coordinated client contact with local health care professionals on a monthly basis.
+• Managed 25 volunteer workers.
+Childcare Experience
+• Coordinated service assignments for 20 part-time counselors and 100 client families.
+• Oversaw daily activity and outing planning for 100 clients.
+• Assisted families of special needs clients with researching financial assistance and
+healthcare.
+• Assisted teachers with managing daily classroom activities.
+• Oversaw daily and special student activities.
+Employment History
+1999-2002 Counseling Supervisor, The Wesley Center, Little Rock, Arkansas.
+1997-1999 Client Specialist, Rainbow Special Care Center, Little Rock, Arkansas
+1996-1997 Teacher’s Assistant, Cowell Elementary, Conway, Arkansas
+Education
+University of Arkansas at Little Rock, Little Rock, AR
+• BS in Early Childhood Development (1999)
+• BA in Elementary Education (1998)
+• GPA (4.0 Scale): Early Childhood Development – 3.8, Elementary Education – 3.5,
+Overall 3.4.
 • Dean’s List, Chancellor’s List"""
 
 
