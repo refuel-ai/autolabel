205962c428e53f1e5d112a6d7e81c703875aad97
Tyler <terrifictyler8@gmail.com>
2023-08-07 11:41:33 -0700

    can't test pdf ocr without tesseract engine

--- tests/unit/transforms/test_pdf_transform.py
+++ tests/unit/transforms/test_pdf_transform.py
@@ -25,27 +25,3 @@ async def test_pdf_transform():
     assert isinstance(transformed_row["metadata"], dict)
     assert len(transformed_row["content"]) > 0
     assert transformed_row["metadata"]["num_pages"] == 1
-
-
-@pytest.mark.asyncio
-async def test_pdf_transform_ocr():
-    # Initialize the PDFTransform class
-    transform = PDFTransform(
-        output_columns={
-            "content_column": "content",
-            "metadata_column": "metadata",
-        },
-        file_path_column="file_path",
-        ocr_enabled=True,
-    )
-
-    # Create a mock row
-    row = {"file_path": "tests/assets/data_loading/Resume.pdf"}
-    # Transform the row
-    transformed_row = await transform.apply(row)
-    # Check the output
-    assert set(transformed_row.keys()) == set(["content", "metadata"])
-    assert isinstance(transformed_row["content"], str)
-    assert isinstance(transformed_row["metadata"], dict)
-    assert len(transformed_row["content"]) > 0
-    assert transformed_row["metadata"]["num_pages"] == 1
