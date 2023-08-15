44f93bdd0393809264a8f746aaa8c8978620049f
Tyler <terrifictyler8@gmail.com>
2023-08-15 16:12:22 -0700

    Synthetic Data Generation With LLMs (#500)
    * draft of what data generation could be
    
    * prints example csv with pandas
    
    * generate labels with the actual autolabel
    
    * added generate dataset to tasks
    
    * remove generator from __init__
    
    * defaults, config, and other comments

--- src/autolabel/tasks/question_answering.py
+++ src/autolabel/tasks/question_answering.py
@@ -99,6 +99,11 @@ class QuestionAnsweringTask(BaseTask):
             labeled_example=fmt_example,
         )
 
+    def get_generate_dataset_prompt(
+        self, label: str, num_rows: int, guidelines: str = None
+    ) -> str:
+        raise NotImplementedError("Dataset generation not implemented for this task")
+
     def eval(
         self,
         llm_labels: List[LLMAnnotation],
