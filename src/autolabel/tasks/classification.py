e3c2126f75bd1296b66510c0dc3abd1ab9a5e737
Patrick Cramer <pcramer247@gmail.com>
2023-09-01 15:00:33 -0700

    replace Chroma DB with autolabels own VectorStoreWrapper. Remove debug prints

--- src/autolabel/tasks/classification.py
+++ src/autolabel/tasks/classification.py
@@ -21,6 +21,11 @@ from autolabel.metrics import (
 
 import json
 
+from langchain.prompts.example_selector import SemanticSimilarityExampleSelector
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import FewShotPromptTemplate, PromptTemplate
+from autolabel.few_shot.vector_store import VectorStoreWrapper
+
 
 class ClassificationTask(BaseTask):
     DEFAULT_OUTPUT_GUIDELINES = (
@@ -56,43 +61,38 @@ class ClassificationTask(BaseTask):
         labels_list = self.config.labels_list()
         num_labels = len(labels_list)
 
-        # if large number of labels, filter by similarity to input
+        # if large number of labels, filter labels_list by similarity of labels to input
         if num_labels >= 50:
-            from langchain.prompts.example_selector import SemanticSimilarityExampleSelector
-            from langchain.vectorstores import Chroma
-            from langchain.embeddings import OpenAIEmbeddings
-            from langchain.prompts import FewShotPromptTemplate, PromptTemplate
-            print(f"PC_DEBUG :: input = {input}")
             example_prompt = PromptTemplate(
                 input_variables=["input"],
                 template="{input}",
             )
-            examples = [{"input":label} for label in labels_list]
+            label_examples = [{"input": label} for label in labels_list]
 
             example_selector = SemanticSimilarityExampleSelector.from_examples(
-                # This is the list of examples available to select from.
-                examples,
+                # This is the list of labels available to select from.
+                label_examples,
                 # This is the embedding class used to produce embeddings which are used to measure semantic similarity.
                 OpenAIEmbeddings(),
                 # This is the VectorStore class that is used to store the embeddings and do a similarity search over.
-                Chroma,
+                VectorStoreWrapper(cache=False),
                 # This is the number of examples to produce.
-                k=5
+                k=10,
             )
             similar_prompt = FewShotPromptTemplate(
-                # We provide an ExampleSelector instead of examples.
                 example_selector=example_selector,
                 example_prompt=example_prompt,
-                prefix="Input: {example}\n",
+                prefix="Input: {example}",
                 suffix="",
                 input_variables=["example"],
             )
-            print(similar_prompt.format(example=input["example"]))
-            #sampled_labels = similar_prompt.format(example=input["example"])
-            #print(sampled_labels)
-            #print(type(sampled_labels))
-            #print(len(sampled_labels))
-            exit()
+            sampled_labels = similar_prompt.format(example=input["example"])
+            split_lines = sampled_labels.split("\n")
+            labels_list = []
+            for i in range(1, len(split_lines)):
+                if split_lines[i]:
+                    labels_list.append(split_lines[i])
+            num_labels = len(labels_list)
 
         fmt_task_guidelines = self.task_guidelines.format(
             num_labels=num_labels, labels="\n".join(labels_list)
