3dc5dda27ca918be9087d012c411371901935d6f
Rajas Bansal <rajas@refuel.ai>
2023-09-11 14:08:01 -0700

    Allow confidence computation per attribute and setting label columns

--- src/autolabel/labeler.py
+++ src/autolabel/labeler.py
@@ -29,6 +29,7 @@ from autolabel.schema import (
     MetricResult,
     TaskRun,
     TaskStatus,
+    TaskType,
 )
 from autolabel.tasks import TaskFactory
 from autolabel.utils import (
@@ -88,9 +89,10 @@ class LabelingAgent:
         self.llm: BaseModel = ModelFactory.from_config(
             self.config, cache=self.generation_cache
         )
-        self.confidence = ConfidenceCalculator(
-            score_type="logprob_average", llm=self.llm
-        )
+        score_type = "logprob_average"
+        if self.config.task_type() == TaskType.ATTRIBUTE_EXTRACTION:
+            score_type = "logprob_average_per_key"
+        self.confidence = ConfidenceCalculator(score_type=score_type, llm=self.llm)
         self.example_selector = example_selector
 
         # Only used if we don't use task management
