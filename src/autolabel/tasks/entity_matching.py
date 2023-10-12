b2a1d3ff48bfe6adf3bbc757e47f04916976d17f
Rajas Bansal <rajasbansal1998@gmail.com>
2023-10-11 18:55:08 -0700

    Update entity_matching.py

--- src/autolabel/tasks/entity_matching.py
+++ src/autolabel/tasks/entity_matching.py
@@ -52,7 +52,7 @@ class EntityMatchingTask(BaseTask):
         for label in self.config.labels_list():
             if "\n" in label:
                 logger.warning(
-                    "Label contains newline character. This can have output guidine issues."
+                    "Label contains newline character. This can have output guideline issues."
                 )
 
     def construct_prompt(self, input: Dict, examples: List[Dict]) -> str:
