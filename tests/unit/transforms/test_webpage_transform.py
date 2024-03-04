2568327f6604b5420404cfaf8b50eb6deafda7e8
Vaibhav Agrawal <vaibhav.ag.001@gmail.com>
2024-03-04 11:05:48 -0800

    reverted to normal transform error

--- tests/unit/transforms/test_webpage_transform.py
+++ tests/unit/transforms/test_webpage_transform.py
@@ -45,11 +45,11 @@ async def test_error_handling():
     transformed_row = await transform.apply(row)
     # Check the output
     assert set(transformed_row.keys()) == set(
-        ["webpage_content", "TransformType.WEBPAGE_TRANSFORM_error"]
+        ["webpage_content", "webpage_scrape_error"]
     )
     assert transformed_row["webpage_content"] == "NO_TRANSFORM"
     assert (
-        transformed_row["TransformType.WEBPAGE_TRANSFORM_error"]
+        transformed_row["webpage_scrape_error"]
         == "Request URL is missing an 'http://' or 'https://' protocol."
     )
 
