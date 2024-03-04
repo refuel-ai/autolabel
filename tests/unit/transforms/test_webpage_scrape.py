de9f9e8922168b2b623b9349c89afc75a660e55c
Abhinav N <114095553+Abhinav-Naikawadi@users.noreply.github.com>
2024-03-04 11:19:20 -0800

    Scrapingbee web scrape support (#734)
    * headless web scraping support
    
    * log level
    
    * delete old
    
    * add timeout and fix https errors
    
    * fix unit tests
    
    * Switch to selenium headless browser for web scraping
    
    * Scrapingbee support for webpage scrape
    
    * req
    
    * logs
    
    * remove selenium
    
    * add sentencepiece
    
    * webscrape error
    
    * restore existing webpage transform
    
    * default timeout
    
    * fix unit tests
    
    * test case
    
    * switch web scrapes
    
    * merge
    
    * fix tests
    
    * duplicate req

---
+++ tests/unit/transforms/test_webpage_scrape.py
@@ -0,0 +1,105 @@
+from autolabel.transforms.webpage_scrape import WebpageScrape
+import pytest
+
+pytest_plugins = ("pytest_asyncio",)
+
+
+@pytest.mark.asyncio
+async def test_webpage_scrape():
+    # Initialize the transform class
+    transform = WebpageScrape(
+        output_columns={
+            "content_column": "webpage_content",
+            "metadata_column": "metadata",
+        },
+        url_column="url",
+        cache=None,
+    )
+
+    # Create a mock row
+    row = {"url": "https://en.wikipedia.org/wiki/Main_Page"}
+    # Transform the row
+    transformed_row = await transform.apply(row)
+    # Check the output
+    assert set(transformed_row.keys()) == set(["webpage_content", "metadata"])
+    assert isinstance(transformed_row["webpage_content"], str)
+    assert isinstance(transformed_row["metadata"], dict)
+    assert len(transformed_row["webpage_content"]) > 0
+
+
+@pytest.mark.asyncio
+async def test_error_handling():
+    # Initialize the transform class
+    transform = WebpageScrape(
+        output_columns={
+            "content_column": "webpage_content",
+        },
+        url_column="url",
+        cache=None,
+    )
+
+    # Create a mock row
+    row = {"url": "bad_url"}
+    # Transform the row
+    transformed_row = await transform.apply(row)
+    # Check the output
+    assert set(transformed_row.keys()) == set(
+        ["webpage_content", "webpage_scrape_error"]
+    )
+    assert transformed_row["webpage_content"] == "NO_TRANSFORM"
+    assert (
+        transformed_row["webpage_scrape_error"]
+        == "Request URL is missing an 'http://' or 'https://' protocol."
+    )
+
+
+@pytest.mark.asyncio
+async def test_empty_url():
+    # Initialize the transform class
+    transform = WebpageScrape(
+        output_columns={
+            "content_column": "webpage_content",
+        },
+        url_column="url",
+        cache=None,
+    )
+
+    # Create a mock row
+    row = {"url": transform.NULL_TRANSFORM_TOKEN}
+    # Transform the row
+    transformed_row = await transform.apply(row)
+    # Check the output
+    assert set(transformed_row.keys()) == set(
+        ["webpage_content", "webpage_scrape_error"]
+    )
+    assert transformed_row["webpage_content"] == "NO_TRANSFORM"
+    assert (
+        transformed_row["webpage_scrape_error"]
+        == "INVALID_INPUT: Empty url in row {'url': 'NO_TRANSFORM'}"
+    )
+
+
+@pytest.mark.asyncio
+async def test_unreachable_url():
+    # Initialize the transform class
+    transform = WebpageScrape(
+        output_columns={
+            "content_column": "webpage_content",
+        },
+        url_column="url",
+        cache=None,
+    )
+
+    # Create a mock row
+    row = {"url": "http://portal.net.kp/"}
+    # Transform the row
+    transformed_row = await transform.apply(row)
+    # Check the output
+    assert set(transformed_row.keys()) == set(
+        ["webpage_content", "webpage_scrape_error"]
+    )
+    assert transformed_row["webpage_content"] == "NO_TRANSFORM"
+    assert (
+        transformed_row["webpage_scrape_error"]
+        == "TRANSFORM_TIMEOUT: Timeout when fetching content from URL"
+    )
