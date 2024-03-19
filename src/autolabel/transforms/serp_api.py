9a7ef4657754e02417bdd7159e5e0c32fb6eaa5d
Abhinav <abhinav.naikawadi@gmail.com>
2024-03-19 09:35:46 -0700

    change severity level to warning

--- src/autolabel/transforms/serp_api.py
+++ src/autolabel/transforms/serp_api.py
@@ -101,7 +101,7 @@ class SerpApi(BaseTransform):
     async def _apply(self, row: Dict[str, Any]) -> Dict[str, Any]:
         for col in self.query_columns:
             if col not in row:
-                logger.error(
+                logger.warning(
                     f"Missing query column: {col} in row {row}",
                 )
         query = self.query_template.format_map(defaultdict(str, row))
