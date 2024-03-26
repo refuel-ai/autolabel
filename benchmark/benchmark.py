75fa1c707d186afea2dfe0e9c0ff9f8a9ad6d32f
Rajas Bansal <rajas@refuel.ai>
2024-03-26 13:26:08 +0000

    changes for benchmarking

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 from rich.console import Console
 
 from autolabel import LabelingAgent, AutolabelConfig, AutolabelDataset
+from autolabel.tasks import TaskFactory
 
 DATASETS = [
     "civil_comments",
@@ -50,23 +51,32 @@ def main():
     eval_file_name = f"eval_{args.model}_{args.few_shot}_{args.max_items}.json"
     eval_file_name = eval_file_name.replace("/", "")
     eval_result = []
+    agent = None
     for dataset in DATASETS:
         config = json.load(open(f"configs/{dataset}.json", "r"))
         config["model"]["name"] = args.model
         config["model"]["provider"] = MODEL_TO_PROVIDER[args.model]
+        config['model']['params'] = {"tensor_parallel_size": 4}
         config["prompt"]["few_shot_num"] = args.few_shot
         if not args.few_shot:
             config["prompt"]["few_shot_selection"] = "fixed"
 
-        agent = LabelingAgent(config, console_output=False, use_tqdm=True)
+        if not agent:
+            agent = LabelingAgent(config, console_output=False, use_tqdm=True)
+        else:
+            config = AutolabelConfig(config)
+            agent.config = config
+            agent.task =  TaskFactory.from_config(config)
+            agent.llm.config = config
+            agent.example_selector = None
         ds = AutolabelDataset(f"data/{dataset}/test.csv", config=config)
         print("Benchmarking", dataset)
         new_ds = agent.run(ds, max_items=args.max_items)
         eval_result.append([x.dict() for x in agent.eval_result])
         json.dump(eval_result, open(eval_file_name, "w"))
         print(eval_result[-1])
-        if config["model"]["provider"] == "vllm":
-            agent.llm.destroy()
+        # if config["model"]["provider"] == "vllm":
+        #     agent.llm.destroy()
 
     print(eval_result)
 
