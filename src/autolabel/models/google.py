25993c6b6e510a301b1bd83b1ec37a69217206ab
Vaibhav Agrawal <vaibhav.ag.001@gmail.com>
2024-03-27 12:06:29 -0700

    added logs

--- src/autolabel/models/google.py
+++ src/autolabel/models/google.py
@@ -75,6 +75,7 @@ class GoogleLLM(BaseModel):
         # populate model params and initialize the LLM
         model_params = config.model_params()
         self.model_params = {**self.DEFAULT_PARAMS, **model_params}
+        logger.info(f"Model Name: {self.model_name}")
         if self._engine == "chat":
             self.llm = ChatGoogleGenerativeAI(
                 model=self.model_name, **self.model_params
