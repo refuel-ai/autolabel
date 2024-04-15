c96dccb339ff3539cd16b34ec0c160b982f8cb29
Vaibhav Agrawal <vaibhav.ag.001@gmail.com>
2024-04-15 14:31:53 -0700

    Added support for Mistral (#800)
    * added mistral support
    
    * added mistral support

--- src/autolabel/schema.py
+++ src/autolabel/schema.py
@@ -22,6 +22,7 @@ class ModelProvider(str, Enum):
     REFUEL = "refuel"
     GOOGLE = "google"
     COHERE = "cohere"
+    MISTRAL = "mistral"
     CUSTOM = "custom"
     TGI = "tgi"
     VLLM = "vllm"
