ce92967ea3d0416da2c6ee49b51697f3896dfca2
Rajas Bansal <rajas@refuel.ai>
2024-04-15 14:20:58 -0700

    Set the tokenizer which is now needed for running refuel llm confidence (#799)
    * Set the tokenizer which is now needed for running refuel llm confidence
    
    * move confidence prompt construction

--- src/autolabel/labeler.py
+++ src/autolabel/labeler.py
@@ -115,7 +115,7 @@ class LabelingAgent:
         if self.config.confidence_chunk_column():
             if not confidence_tokenizer:
                 self.confidence_tokenizer = AutoTokenizer.from_pretrained(
-                    "google/flan-t5-xxl"
+                    "NousResearch/Llama-2-13b-chat-hf"
                 )
             else:
                 self.confidence_tokenizer = confidence_tokenizer
@@ -308,14 +308,6 @@ class LabelingAgent:
                         annotation = self.task.parse_llm_response(
                             generation, chunk, final_prompt
                         )
-                        annotation.confidence_prompt = (
-                            self.task.construct_confidence_prompt(
-                                chunk,
-                                examples,
-                                max_input_tokens=self.CONFIDENCE_MAX_CONTEXT_LENGTH,
-                                get_num_tokens=self.get_num_tokens,
-                            )
-                        )
                         annotation.input_tokens = input_tokens
                         annotation.output_tokens = self.llm.get_num_tokens(
                             annotation.raw_response
@@ -324,6 +316,14 @@ class LabelingAgent:
                         annotation.latency = latency
 
                         if self.config.confidence():
+                            annotation.confidence_prompt = (
+                                self.task.construct_confidence_prompt(
+                                    chunk,
+                                    examples,
+                                    max_input_tokens=self.CONFIDENCE_MAX_CONTEXT_LENGTH,
+                                    get_num_tokens=self.get_num_tokens,
+                                )
+                            )
                             try:
                                 annotation.confidence_score = (
                                     await self.confidence.calculate(
@@ -667,6 +667,11 @@ class LabelingAgent:
         self.transform_cache.clear(use_ttl=use_ttl)
 
     def get_num_tokens(self, inp: str) -> int:
+        if not self.confidence_tokenizer:
+            logger.warning("Confidence tokenizer is not set. Using default tokenizer.")
+            self.confidence_tokenizer = AutoTokenizer.from_pretrained(
+                "NousResearch/Llama-2-13b-chat-hf"
+            )
         """Returns the number of tokens in the prompt"""
         return len(self.confidence_tokenizer.encode(str(inp)))
 
