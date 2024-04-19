78139ee9cf68a208f645cf8b578e697fe3e9ac9e
Vaibhav Agrawal <vaibhav.ag.001@gmail.com>
2024-04-19 13:38:23 -0700

    Added timeout and some cleanup for mistral (#806)
    * added timeout and some cleanup
    
    * black fmt
    
    * minor
    
    * fixed timeout
    
    * black fmt

--- src/autolabel/models/mistral.py
+++ src/autolabel/models/mistral.py
@@ -5,8 +5,6 @@ from time import time
 from typing import List, Optional, Tuple
 import httpx
 
-from langchain.schema import HumanMessage
-
 from autolabel.cache import BaseCache
 from autolabel.configs import AutolabelConfig
 from autolabel.models import BaseModel
@@ -33,10 +31,8 @@ class UnretryableError(Exception):
 
 
 class MistralLLM(BaseModel):
-    DEFAULT_TOKENIZATION_MODEL = "NousResearch/Llama-2-13b-chat-hf"
-    DEFAULT_CONTEXT_LENGTH = 3250
     DEFAULT_CONNECT_TIMEOUT = 10
-    DEFAULT_READ_TIMEOUT = 120
+    DEFAULT_READ_TIMEOUT = 60
     DEFAULT_MODEL = "mistral-small-latest"
     DEFAULT_PARAMS = {
         "max_tokens": 1000,
@@ -62,14 +58,6 @@ class MistralLLM(BaseModel):
     def __init__(self, config: AutolabelConfig, cache: BaseCache = None) -> None:
         super().__init__(config, cache)
 
-        try:
-            # get the tokenizer
-            from langchain_mistralai import ChatMistralAI
-        except ImportError:
-            raise ImportError(
-                "mistralai and langchain_mistralai is required to use the anthropic LLM. Please install it with the following command: pip install 'refuel-autolabel[mistral]'"
-            )
-
         if os.getenv("MISTRAL_API_KEY") is None:
             raise ValueError("MISTRAL_API_KEY environment variable not set")
 
@@ -79,6 +67,10 @@ class MistralLLM(BaseModel):
         self.prompts2tokens = {}
         # populate model params
         model_params = config.model_params()
+        self.timeout = self.DEFAULT_READ_TIMEOUT
+        if "request_timeout" in model_params.keys():
+            self.timeout = model_params["request_timeout"]
+            del model_params["request_timeout"]
         self.model_params = {**self.DEFAULT_PARAMS, **model_params}
         self.url = "https://api.mistral.ai/v1/chat/completions"
 
@@ -101,7 +93,9 @@ class MistralLLM(BaseModel):
             "Authorization": "Bearer " + os.getenv("MISTRAL_API_KEY"),
         }
         start_time = time()
-        response = requests.post(self.url, json=data, headers=headers)
+        response = requests.post(
+            self.url, json=data, headers=headers, timeout=self.timeout
+        )
         end_time = time()
         # raise Exception if status != 200
         if response.status_code != 200:
@@ -130,9 +124,7 @@ class MistralLLM(BaseModel):
             "Authorization": "Bearer " + os.getenv("MISTRAL_API_KEY"),
         }
         async with httpx.AsyncClient() as client:
-            timeout = httpx.Timeout(
-                self.DEFAULT_CONNECT_TIMEOUT, read=self.DEFAULT_READ_TIMEOUT
-            )
+            timeout = httpx.Timeout(self.DEFAULT_CONNECT_TIMEOUT, read=self.timeout)
             start_time = time()
             response = await client.post(
                 self.url, json=data, headers=headers, timeout=timeout
