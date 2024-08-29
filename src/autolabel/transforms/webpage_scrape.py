6309ac2a6c8afa5009bf17e1b5c34eda6b48dc0e
Abhinav <abhinav.naikawadi@gmail.com>
2024-08-28 17:11:34 -0700

    validate webpage scrape URL's

--- src/autolabel/transforms/webpage_scrape.py
+++ src/autolabel/transforms/webpage_scrape.py
@@ -1,3 +1,4 @@
+from autolabel.utils import validate_url
 from autolabel.transforms.schema import (
     TransformType,
     TransformError,
@@ -142,6 +143,11 @@ class WebpageScrape(BaseTransform):
         else:
             if not urlparse(url).scheme:
                 url = f"https://{url}"
+            if not validate_url(url):
+                raise TransformError(
+                    TransformErrorType.INVALID_INPUT,
+                    f"Invalid url in row {row}",
+                )
             url_response_data = await self._load_url(url)
 
         transformed_row = {
